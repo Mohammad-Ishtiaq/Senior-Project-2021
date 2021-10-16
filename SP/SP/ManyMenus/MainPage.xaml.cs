df0d250948e10233115df205fabf0e3d213dcee3
Mohammad Ishtiaq <ishtiaqm31@gmail.com>
2021-10-15 21:11:41 -0400

    Pages Addidition

---
+++ SP/SP/ManyMenus/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using SP.ManyMenus;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void SymptomCheckerButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new symptomChecker());
+        }
+
+        private void OTCCheckerButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new OTCCheckerButton());
+        }
+
+        private void CovidCheckerButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CovidCheckerButton());
+        }
+    }
+}
