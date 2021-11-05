a4ac602c128fb8b9bbea566d6f5307c838d8b49e
NusratBainya <91344945+NusraBainya@users.noreply.github.com>
2021-11-05 09:30:09 -0400

    Add files via upload

---
+++ SP/SP/Models/Loc.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SP.Models
+{
+    public class Loc
+    {
+        public List<double> coordinates { get; set; }
+        public string type { get; set; }
+    }
+}
---
+++ SP/SP/Models/Root.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SP.Models
+{
+    public class Root
+    {
+        public string _id { get; set; }
+        public string combined_name { get; set; }
+        public int confirmed { get; set; }
+        public int confirmed_daily { get; set; }
+        public string country { get; set; }
+        public int country_code { get; set; }
+        public string country_iso2 { get; set; }
+        public string country_iso3 { get; set; }
+        public string county { get; set; }
+        public DateTime date { get; set; }
+        public int deaths { get; set; }
+        public int deaths_daily { get; set; }
+        public int fips { get; set; }
+        public Loc loc { get; set; }
+        public int population { get; set; }
+        public string state { get; set; }
+        public int uid { get; set; }
+    }
+}
